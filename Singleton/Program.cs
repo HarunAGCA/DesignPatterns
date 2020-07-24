293ae0c5a39f384770d36f97a9f451884cab74be
Harun AGCA <harun39117@gmail.com>
2020-07-25 00:35:48 +0300

    Added Singleton DP

---
+++ Singleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = Manager.CreateAsSingleton();
+            manager.TestAction();
+                
+        }
+    }
+
+    class Manager
+    {
+        private static Manager _manager;
+        private static object _lockObject = new object();
+
+        private Manager()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates thread safe singleton instance
+        /// </summary>
+        /// <returns>Manager</returns>
+        public static Manager CreateAsSingleton()
+        {
+            lock (_lockObject)  //lock for thread safing
+
+            {
+                if (_manager == null)
+                {
+                    _manager = new Manager();
+                }
+            }
+
+            return _manager;
+        }
+        /// <summary>
+        /// Represents any action. Created for testing purpose.
+        /// </summary>
+        public void TestAction()
+        {
+            Console.WriteLine("Test action ran.");
+        }
+    }
+}
